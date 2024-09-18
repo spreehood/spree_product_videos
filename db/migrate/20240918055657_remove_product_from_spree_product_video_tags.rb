65b82f5f4c257ae8184e4aa94c76068f88b41e49
Mausam Pun <sonicmausam@gmail.com>
2024-09-18 16:31:14 +0545

    fix the migration version number

--- db/migrate/20240918055657_remove_product_from_spree_product_video_tags.rb
+++ db/migrate/20240918055657_remove_product_from_spree_product_video_tags.rb
@@ -1,4 +1,4 @@
-class RemoveProductFromSpreeProductVideoTags < ActiveRecord::Migration[7.2]
+class RemoveProductFromSpreeProductVideoTags < ActiveRecord::Migration[7.1]
   def change
     remove_reference :spree_product_video_tags, :product, foreign_key: { to_table: :spree_products }, index: true
   end
