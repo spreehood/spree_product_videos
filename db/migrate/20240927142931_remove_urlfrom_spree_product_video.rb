192348ac2935758b1ded329d18a51bb5ed16ff0f
Mausam Pun <sonicmausam@gmail.com>
2024-09-27 21:14:34 +0545

    fix the migration version number

--- db/migrate/20240927142931_remove_urlfrom_spree_product_video.rb
+++ db/migrate/20240927142931_remove_urlfrom_spree_product_video.rb
@@ -1,4 +1,4 @@
-class RemoveUrlfromSpreeProductVideo < ActiveRecord::Migration[7.2]
+class RemoveUrlfromSpreeProductVideo < ActiveRecord::Migration[7.1]
   def change
     remove_column :spree_product_videos, :url
   end
