f9c039ae31e7599a3c0bf653bc10eee54464e6eb
Sushil Subedi <sushilsubedi151@gmail.com>
2024-09-30 14:15:49 +0545

    fixed video serializer

--- app/serializers/spree/api/v2/video_serializer.rb
+++ app/serializers/spree/api/v2/video_serializer.rb
@@ -5,7 +5,11 @@ module Spree
         attributes :id, :product_id, :created_at
 
         attribute :url do |object|
-          Rails.application.routes.url_helpers.rails_blob_url(object.file, only_path: true)
+          if object.file.attached?
+            Rails.application.routes.url_helpers.rails_blob_url(object.file, only_path: true)
+          else
+            nil 
+          end
         end
 
         has_many :tags, serializer: :tag
